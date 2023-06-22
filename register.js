c863ff1412db35301d4038fb0da044a32fa0cef0
Michal Massachi <michal2792000@gmail.com>
2023-06-22 17:24:53 +0300

    updated login and register

---
+++ register.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $(".toggle-password").click(function() {
+      $(this).toggleClass('active');
+      var passwordInput = $(this).prev("input");
+      if (passwordInput.attr("type") === "password") {
+        passwordInput.attr("type", "text");
+      } else {
+        passwordInput.attr("type", "password");
+      }
+    });
+  });
+  
+
+  $(document).ready(function() {
+    $('form').on('submit', function(event) {
+      var username = $('#username').val();
+      var password = $('#password').val();
+      var passwordValidation = $('#password-validation').val();
+      
+      if (username.length < 5 || password.length < 5 || passwordValidation.length < 5) {
+        event.preventDefault(); // Prevent form submission
+        
+        if (username.length < 5) {
+          $('#username-error').addClass('error-message').text('Username must be at least 5 characters long (including numbers).'); //For Example, will be changed
+        } else {
+          $('#username-error').removeClass('error-message').text('');
+        }
+        
+        if (password.length < 5) {
+          $('#password-error').addClass('error-message').text('Password must be at least 5 characters long (including numbers).');
+        } else {
+          $('#password-error').removeClass('error-message').text('');
+        }
+        
+        if (passwordValidation.length < 5) {
+          $('#password-validation-error').addClass('error-message').text('Password validation must be at least 5 characters long (including numbers).');
+        } else {
+          $('#password-validation-error').removeClass('error-message').text('');
+        }
+      }
+    });
+  });
