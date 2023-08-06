100b1b20cc2f8132f1e884a4c42cb03a344dcb5e
Arik Leikin <arik.leikin1998@gmail.com>
2023-08-06 11:53:57 +0300

    Added static to express + homepage get method

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const router = express.Router();
-const homepage = require("./routes/homepage");
+const homepage = require("./routes/homepage.routes");
 
-app.use("/", homepage);
+app.use(express.static("public"));
+app.get("/", homepage);
 
 app.listen(process.env.PORT, () => {
   console.log("Server started");
--- routes/homepage.routes.js
+++ routes/homepage.routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "../html/main.html");
+  const filePath = path.join(__dirname, "../public/html/main.html");
   console.log(filePath);
   res.sendFile(filePath);
 });
