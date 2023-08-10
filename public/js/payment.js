1a87a9f96c73b9ae3303648bf94ee420c5e4dfc7
Michal Massachi <michal2792000@gmail.com>
2023-08-10 15:10:45 +0300

    updated functionality of cart and payment

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,207 +1,173 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const queryParams = new URLSearchParams(window.location.search);
-  const total = queryParams.get("total");
+  const totalElement = document.querySelector("#total-price"); // Update this selector to match your payment page's total element
 
-  if (total) {
-    document.getElementById("total-price").textContent = "Total(USD) $" + total;
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const cartTotal = urlParams.get("total");
+
+  if (cartTotal !== null) {
+    totalElement.textContent = "Total(USD) $" + cartTotal;
   }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const total = sessionStorage.getItem("cartTotal");
 
-  if (total) {
-    document.getElementById("total-price").textContent =
-      "Total Price: $" + total;
-  }
-});
+  $(document).ready(function () {
 
-$(document).ready(function () {
-  $("form").on("submit", function (event) {
-    var Name = $("#name").val();
-    var Id = $("#id").val();
-    var Street = $("#street").val();
-    var StreetNumber = $("#street-number").val();
-    var City = $("#city").val();
-    var CreditCard = $("#credit-card").val();
-    var CVV = $("#cvv").val();
-    var Phone = $("#phone").val();
-
-    var validateName = isOnlyLetters(Name);
-    var validateId = hasSpecificNumberOfDigits(Id, 9);
-    var validateStreet = isOnlyLetters(Street);
-    var validateStreetNumber = containsOnlyNumbers(StreetNumber);
-    var validateCity = isOnlyLetters(City);
-    var validateCreditCard = hasSpecificNumberOfDigits(CreditCard, 16);
-    var validateCVV = hasSpecificNumberOfDigits(CVV, 3);
-    var validatePhone = hasSpecificNumberOfDigits(Phone, 10);
-
-    if (!validateName) {
+    $("#purchase-form").submit(function (event) {
       event.preventDefault();
-      $("#username-error")
-        .addClass("error-message")
-        .text("Name should contain only letters.");
-    } else {
-      $("#username-error").removeClass("error-message").text("");
-    }
+      clearErrorMessages();
 
-    if (!validatePhone) {
-      event.preventDefault();
-      $("#phone-error")
-        .addClass("error-message")
-        .text("Phone number should be 10 digits.");
-    } else {
-      $("#phone-error").removeClass("error-message").text("");
-    }
+      let isValid = true;
 
-    if (!validateId) {
-      event.preventDefault();
-      $("#id-error").addClass("error-message").text("ID should be 9 digits.");
-    } else {
-      $("#id-error").removeClass("error-message").text("");
-    }
+      const phone = $("#phone").val();
+      if (!validatePhoneNumber(phone)) {
+        showError("#phone-error", "Phone number must be 10 digits.");
+        isValid = false;
+      }
 
-    if (!validateStreet) {
-      event.preventDefault();
-      $("#street-error")
-        .addClass("error-message")
-        .text("Street should contain only letters.");
-    } else {
-      $("#street-error").removeClass("error-message").text("");
-    }
+      const city = $("#city").val();
+      if (!validateCity(city)) {
+        showError("#city-error", "City name can only contain letters.");
+        isValid = false;
+      }
 
-    if (!validateStreetNumber) {
-      event.preventDefault();
-      $("#street-number-error")
-        .addClass("error-message")
-        .text("Street Number should contain only numbers.");
-    } else {
-      $("#street-number-error").removeClass("error-message").text("");
-    }
+      const street = $("#street").val();
+      if (!validateStreet(street)) {
+        showError("#street-error", "Street name can only contain letters.");
+        isValid = false;
+      }
 
-    if (!validateCity) {
-      event.preventDefault();
-      $("#city-error")
-        .addClass("error-message")
-        .text("City should contain only letters.");
-    } else {
-      $("#city-error").removeClass("error-message").text("");
-    }
+      const streetNumber = $("#street_number").val();
+      if (!validateStreetNumber(streetNumber)) {
+        showError("#street-number-error", "Street number must be digits.");
+        isValid = false;
+      }
 
-    if (!validateCreditCard) {
-      event.preventDefault();
-      $("#credit-card-error")
-        .addClass("error-message")
-        .text("Credit Card should be 16 digits.");
-    } else {
-      $("#credit-card-error").removeClass("error-message").text("");
-    }
+      const cardNumber = $("#card-number").val();
+      const cardNumber1 = $("#card-number-1").val();
+      const cardNumber2 = $("#card-number-2").val();
+      const cardNumber3 = $("#card-number-3").val();
+      
+      if (!validateCardNumber(cardNumber) ||!validateCardNumber(cardNumber1) || !validateCardNumber(cardNumber2) || !validateCardNumber(cardNumber3)) {
+        showError("#card-number-error", "Card number must have 4 digits each.");
+        isValid = false;
+      }
 
-    if (!validateCVV) {
-      event.preventDefault();
-      $("#cvv-number-error")
-        .addClass("error-message")
-        .text("CVV should be 3 digits.");
-    } else {
-      $("#cvv-number-error").removeClass("error-message").text("");
-    }
+      const cardHolder = $("#card-holder").val();
+      if (!validateCardHolder(cardHolder)) {
+        showError("#card-holder-error", "Card holder name must have 9 digits.");
+        isValid = false;
+      }
 
-    var selectedMonth = parseInt($("#expiration-month").val());
-    var selectedYear = parseInt($("#expiration-year").val());
+      const expirationMonth = $("#card-expiration-month").val();
+      const expirationYear = $("#card-expiration-year").val();
+      if (!validateExpirationDate(expirationMonth, expirationYear)) {
+        showError("#expiration-month-error", "Invalid expiration date.");
+        isValid = false;
+      }
 
-    // Get the current date
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    var currentMonth = currentDate.getMonth() + 1; // Month is zero-based
+      const cvv = $("#card-ccv").val();
+      if (!validateCVV(cvv)) {
+        showError("#cvv-number-error", "CVV must be 3 digits.");
+        isValid = false;
+      }
 
-    // Compare the expiration date with the current date
-    if (
-      selectedYear < currentYear ||
-      (selectedYear === currentYear && selectedMonth < currentMonth)
-    ) {
-      event.preventDefault();
-      $("#expiration-date-error")
-        .addClass("error-message")
-        .text("Expiration date is not valid.");
-    } else {
-      $("#expiration-date-error").removeClass("error-message").text("");
-    }
+      if (isValid) {
+        const phone = $("#phone").val();
+        const city = $("#city").val();
+        const street = $("#street").val();
+        const streetNumber = $("#street_number").val();
+        const cardHolder = $("#card-holder").val();
+        const expirationMonth = $("#card-expiration-month").val();
+        const expirationYear = $("#card-expiration-year").val();
 
-    if (
-      validateName &&
-      validateId &&
-      validateStreet &&
-      validateStreetNumber &&
-      validateCity &&
-      validateCreditCard &&
-      validateCVV &&
-      validatePhone &&
-      selectedYear >= currentYear &&
-      selectedMonth >= currentMonth
-    ) {
-      event.preventDefault(); // Prevent the form from submitting
+        const totalElement = document.querySelector("#total-price"); 
+    
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const cartTotal = urlParams.get("total");
+    
+      if (cartTotal !== null) {
+        totalElement.textContent = "Total(USD) $" + cartTotal;
+      }
+        
+        const modalContent = `
+          <h2>Thank you for your purchase! We hope you will visit us again.</h2>
+          <h2>Your Purchase Details:</h2>
+          <p><strong>Phone Number:</strong> ${phone}</p>
+          <p><strong>City:</strong> ${city}</p>
+          <p><strong>Street:</strong> ${street}</p>
+          <p><strong>Street Number:</strong> ${streetNumber}</p>
+          <p><strong>Card Holder:</strong> ${cardHolder}</p>
+          <p><strong>Expiration Date:</strong> ${expirationMonth}/${expirationYear}</p>
+          <p><strong>Total Amount:</strong> $${cartTotal}</p>
+        `;
 
-      // Get input values
-      var name = $("#name").val();
-      var phone = $("#phone").val();
-      var id = $("#id").val();
-      var city = $("#city").val();
-      var street = $("#street").val();
-      var streetNumber = $("#street-number").val();
-      var creditCard = $("#credit-card").val();
-      var cvv = $("#cvv").val();
-      var expirationMonth = $("#expiration-month").val();
-      var expirationYear = $("#expiration-year").val();
-      var totalPrice = $("#total-price").text();
+        $("#modal-content").html(modalContent);
 
-      // Build modal content
-      var modalContent = `
-            <h2>Thank you for your purchas! We hope you will visit us again.</h2>
-              <h2>Your Purches Details:</h2>
-              <p><strong>Name:</strong> ${name}</p>
-              <p><strong>Phone:</strong> ${phone}</p>
-              <p><strong>ID:</strong> ${id}</p>
-              <p><strong>City:</strong> ${city}</p>
-              <p><strong>Street:</strong> ${street}</p>
-              <p><strong>Street Number:</strong> ${streetNumber}</p>
-              <p><strong>Credit Card:</strong> ${creditCard}</p>
-              <p><strong>CVV:</strong> ${cvv}</p>
-              <p><strong>Expiration Date:</strong> ${expirationMonth}/${expirationYear}</p>
-              <p><strong style="color: green;"></strong> <span style="color: green; font-weight: bold;">${totalPrice}</span></p>
-             
-             
-            `;
-
-      // Set modal content
-      $("#modal-content").html(modalContent);
-
-      // Show the modal
-      $("#myModal").show();
-    }
-
-    // Close the modal when the close button is clicked
-    $(".close").click(function () {
-      $("#myModal").hide();
+        $("#myModal").show();
+      }
     });
+
+      $(".close").click(function () {
+        $("#myModal").hide();
+      });
   });
-});
 
-function isOnlyLetters(inputString) {
-  // Regular expression to match only letters (uppercase and lowercase)
-  var letterRegex = /^[A-Za-z]+$/;
 
-  // Test the input string against the regular expression
-  return letterRegex.test(inputString);
-}
+    function validatePhoneNumber(phone) {
+      return /^\d{10}$/.test(phone);
+    }
 
-function containsOnlyNumbers(str) {
-  return /^\d+$/.test(str);
-}
+    function validateCity(city) {
+      return /^[A-Za-z\s]+$/.test(city);
+    }
 
-function hasSpecificNumberOfDigits(inputString, numberOfDigits) {
-  // Regular expression to match the specified number of digits
-  var digitRegex = new RegExp(`^[0-9]{${numberOfDigits}}$`);
+    function validateStreet(street) {
+      return /^[A-Za-z\s]+$/.test(street);
+    }
 
-  // Test the input string against the regular expression
-  return digitRegex.test(inputString);
-}
+    function validateStreetNumber(streetNumber) {
+      return /^\d+$/.test(streetNumber);
+    }
+
+    function validateCardNumber(cardNumber) {
+      return /^\d{4}$/.test(cardNumber);
+    }
+
+    function validateCardHolder(cardHolder) {
+      return /^\d{9}$/.test(cardHolder);
+    }
+
+    function validateExpirationDate(expirationMonth, expirationYear) {
+      const currentYear = new Date().getFullYear();
+      const currentMonth = new Date().getMonth() + 1;
+      const enteredYear = parseInt(expirationYear, 10);
+      const enteredMonth = parseInt(expirationMonth, 10);
+
+      if (enteredYear < currentYear) {
+        return false;
+      } else if (enteredYear === currentYear && enteredMonth < currentMonth) {
+        return false;
+      }
+      return true;
+    }
+
+    function validateCVV(cvv) {
+      return /^\d{3}$/.test(cvv);
+    }
+
+    function showError(element, message) {
+      $(element).text(message).css("color", "red");
+    }
+
+    function clearErrorMessages() {
+      $(".error-message").text("");
+    }
+  
+
+
+
+
+
+
