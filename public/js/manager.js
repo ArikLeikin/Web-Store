f8374fa83c73a41da000924ee131ccdad149ba3a
Michal Massachi <michal2792000@gmail.com>
2023-08-21 14:15:09 +0300

    add user

--- public/js/manager.js
+++ public/js/manager.js
@@ -72,97 +72,97 @@
    
 //   });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const userTable = document.getElementById("userTable");
-    const addUserBtn = document.getElementById("addUserBtn");
-    const newEmailInput = document.getElementById("newEmail");
-    const newPasswordInput = document.getElementById("newPassword");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const userTable = document.getElementById("userTable");
+//     const addUserBtn = document.getElementById("addUserBtn");
+//     const newEmailInput = document.getElementById("newEmail");
+//     const newPasswordInput = document.getElementById("newPassword");
   
-    // Sample initial user data
-    const initialUsers = [
-      { email: "user1@example.com", password: "password123" },
-      { email: "user2@example.com", password: "securepass" },
-      // Add more initial users as needed
-    ];
+//     // Sample initial user data
+//     const initialUsers = [
+//       { email: "user1@example.com", password: "password123" },
+//       { email: "user2@example.com", password: "securepass" },
+//       // Add more initial users as needed
+//     ];
   
-    // Populate the table with initial user data
-    initialUsers.forEach(user => {
-      addUserToTable(user.email, user.password);
-    });
+//     // Populate the table with initial user data
+//     initialUsers.forEach(user => {
+//       addUserToTable(user.email, user.password);
+//     });
   
-    // Function to add a user to the table
-    function addUserToTable(email, password) {
-      const newRow = userTable.insertRow();
-      newRow.innerHTML = `
-        <td>${email}</td>
-        <td>${password}</td>
-        <td><button class="deleteBtn">Delete</button></td>
-      `;
+//     // Function to add a user to the table
+//     function addUserToTable(email, password) {
+//       const newRow = userTable.insertRow();
+//       newRow.innerHTML = `
+//         <td>${email}</td>
+//         <td>${password}</td>
+//         <td><button class="deleteBtn">Delete</button></td>
+//       `;
   
-      const deleteBtn = newRow.querySelector(".deleteBtn");
-      deleteBtn.addEventListener("click", function() {
-        userTable.deleteRow(newRow.rowIndex);
-      });
-    }
+//       const deleteBtn = newRow.querySelector(".deleteBtn");
+//       deleteBtn.addEventListener("click", function() {
+//         userTable.deleteRow(newRow.rowIndex);
+//       });
+//     }
   
-    // Add user button click event
-    addUserBtn.addEventListener("click", function() {
-      const newEmail = newEmailInput.value;
-      const newPassword = newPasswordInput.value;
-      if (newEmail && newPassword) {
-        addUserToTable(newEmail, newPassword);
-        newEmailInput.value = "";
-        newPasswordInput.value = "";
-      }
-    });
-  });
+//     // Add user button click event
+//     addUserBtn.addEventListener("click", function() {
+//       const newEmail = newEmailInput.value;
+//       const newPassword = newPasswordInput.value;
+//       if (newEmail && newPassword) {
+//         addUserToTable(newEmail, newPassword);
+//         newEmailInput.value = "";
+//         newPasswordInput.value = "";
+//       }
+//     });
+//   });
 
   
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    const SupplierTable = document.getElementById("supplierTable");
-    const addSupplierBtn = document.getElementById("addSupplierBtn");
-    const newEmailInput = document.getElementById("newEmail");
-    const newPasswordInput = document.getElementById("newPassword");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const SupplierTable = document.getElementById("supplierTable");
+//     const addSupplierBtn = document.getElementById("addSupplierBtn");
+//     const newEmailInput = document.getElementById("newEmail");
+//     const newPasswordInput = document.getElementById("newPassword");
   
-    // Sample initial user data
-    const initialUsers = [
-      { email: "supplier@example.com", password: "password123" },
-      { email: "supplier2@example.com", password: "securepass" },
-      // Add more initial users as needed
-    ];
+//     // Sample initial user data
+//     const initialUsers = [
+//       { email: "supplier@example.com", password: "password123" },
+//       { email: "supplier2@example.com", password: "securepass" },
+//       // Add more initial users as needed
+//     ];
   
-    // Populate the table with initial user data
-    initialUsers.forEach(user => {
-        addSupplierToTable(user.email, user.password);
-    });
+//     // Populate the table with initial user data
+//     initialUsers.forEach(user => {
+//         addSupplierToTable(user.email, user.password);
+//     });
   
-    // Function to add a user to the table
-    function addSupplierToTable(email, password) {
-      const newRow = SupplierTable.insertRow();
-      newRow.innerHTML = `
-        <td>${email}</td>
-        <td>${password}</td>
-        <td><button class="deleteBtn">Delete</button></td>
-      `;
+//     // Function to add a user to the table
+//     function addSupplierToTable(email, password) {
+//       const newRow = SupplierTable.insertRow();
+//       newRow.innerHTML = `
+//         <td>${email}</td>
+//         <td>${password}</td>
+//         <td><button class="deleteBtn">Delete</button></td>
+//       `;
   
-      const deleteBtn = newRow.querySelector(".deleteBtn");
-      deleteBtn.addEventListener("click", function() {
-        SupplierTable.deleteRow(newRow.rowIndex);
-      });
-    }
+//       const deleteBtn = newRow.querySelector(".deleteBtn");
+//       deleteBtn.addEventListener("click", function() {
+//         SupplierTable.deleteRow(newRow.rowIndex);
+//       });
+//     }
   
-    // Add user button click event
-    addSupplierBtn.addEventListener("click", function() {
-      const newEmail = newEmailInput.value;
-      const newPassword = newPasswordInput.value;
-      if (newEmail && newPassword) {
-        addSupplierToTable(newEmail, newPassword);
-        newEmailInput.value = "";
-        newPasswordInput.value = "";
-      }
-    });
-  });
+//     // Add user button click event
+//     addSupplierBtn.addEventListener("click", function() {
+//       const newEmail = newEmailInput.value;
+//       const newPassword = newPasswordInput.value;
+//       if (newEmail && newPassword) {
+//         addSupplierToTable(newEmail, newPassword);
+//         newEmailInput.value = "";
+//         newPasswordInput.value = "";
+//       }
+//     });
+//   });
 
 
