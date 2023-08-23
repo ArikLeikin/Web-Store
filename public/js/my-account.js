0167d1be51b630e5e345f6e080c79e89d12f4290
Nofar Shapir <nofarshapir00@gmail.com>
2023-08-23 10:22:14 +0300

    fix bugs

--- public/js/my-account.js
+++ public/js/my-account.js
@@ -45,53 +45,6 @@ $(document).ready(function () {
   }
 });
 
-// $(document).ready(function () {
-//   function show(showPage) {
-//     var order_history = document.getElementById("order-history");
-//     var personal_details = document.getElementById("personal-details");
-//     var change_password = document.getElementById("change-password");
-//     var addresses = document.getElementById("addresses");
-//     var payment_methods = document.getElementById("payment-methods");
-//     var wish_list = document.getElementById("wish-list");
-//     var yad2_list = document.getElementById("yad2-list");
-
-//     order_history.style.display = "none";
-//     personal_details.style.display = "none";
-//     change_password.style.display = "none";
-//     addresses.style.display = "none";
-//     payment_methods.style.display = "none";
-//     wish_list.style.display = "none";
-//     yad2_list.style.display = "none";
-
-//     switch (showPage) {
-//       case "order_history":
-//         order_history.style.display = "block";
-//         break;
-//       case "personal_details":
-//         personal_details.style.display = "block";
-//         break;
-//       case "change_password":
-//         change_password.style.display = "block";
-//         break;
-//       case "addresses":
-//         addresses.style.display = "block";
-//         break;
-//       case "payment_methods":
-//         payment_methods.style.display = "block";
-//         break;
-//       case "wish_list":
-//         wish_list.style.display = "block";
-//         break;
-//       case "yad2_list":
-//         yad2_list.style.display = "block";
-//         break;
-//       default:
-//         order_history.style.display = "block";
-//         break;
-//     }
-//   }
-// });
-// Define the show function in the global scope
 function show(showPage) {
   $(".page-section").hide();
   $("#" + showPage).show();
