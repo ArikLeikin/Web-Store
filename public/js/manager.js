b096c6f4df83e87ccd32d6506ffa0665c290d99e
Michal Massachi <michal2792000@gmail.com>
2023-08-27 14:32:14 +0300

    manager and payment

--- public/js/manager.js
+++ public/js/manager.js
@@ -1,168 +1,78 @@
+// $(document).ready(function () {
+//     $("#add-user-form").submit(function (event) {
+//       event.preventDefault();
+//       let isValid = true;
+  
+//       const FirstName = $("#firstname").val();
+//       if (!validateName(FirstName)) {
+//         // Swal.fire({
+//         //   icon: "warning",
+//         //   title: "Validation Error",
+//         //   text: "First name can only contain letters.",
+//         // });
+//          showError("#firstname-error", "First name can only contain letters.");
+//         isValid = false;
+//       }
+  
+//       const LastName = $("#lastname").val();
+//       if (!validateName(LastName)) {
+//         // Swal.fire({
+//         //   icon: "warning",
+//         //   title: "Validation Error",
+//         //   text: "Last name can only contain letters.",
+//         // });
+//         showError("#lastname-error", "Last name can only contain letters.");
+//         isValid = false;
+//       }
+  
+//       const phone = $("#phoneNumber").val();
+//       if (!validatePhoneNumber(phone)) {
+//         // Swal.fire({
+//         //   icon: "warning",
+//         //   title: "Validation Error",
+//         //   text: "Phone number must be 10 digits.",
+//         // });
+//         showError("#phoneNumber-error", "Phone number must be 10 digits.");
+//         isValid = false;
+//       }
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     createEmployeeList();
-//     const userManagementSection = document.getElementById("user-management");
-//     const supplierManagementSection = document.getElementById("supplier-management");
-//     const contentManagementSection = document.getElementById("content-management");
+//       const password = $("#password").val();
+//       var passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/;
+//     if (!passwordPattern.test(password)) {
+//       // Swal.fire({
+//       //   icon: "error",
+//       //   title: "Invalid Password",
+//       //   text: "Password must contain at least 8 characters, including 1 number, 1 uppercase letter, 1 lowercase letter, and 1 special character.",
+//       // });
+//       showError("#password-error", "Password must contain at least 8 characters, including 1 number, 1 uppercase letter, 1 lowercase letter, and 1 special character.");
+//       isValid = false;
+//     }
 
-//     userManagementSection.addEventListener("click", handleUserManagement);
-//     supplierManagementSection.addEventListener("click", handleSupplierManagement);
-//     contentManagementSection.addEventListener("click", handleContentManagement);
+//     // if (!isValid) {
+//     //   event.returnValue = false; 
+//     //   }
+
+//     });
 // });
 
-// document.addEventListener("DOMContentLoaded", function () {
-// // Sample user data
-// const users = [
-//     { email: "user1@example.com", password: "password1" },
-//     { email: "user2@example.com", password: "password2" },
-//     { email: "user3@example.com", password: "password3" }
-// ];
-
-// const userTable = document.querySelector("#user-table tbody");
-
-
-// function createUserTable() {
-//     users.forEach((user, index) => {
-//         const row = document.createElement("tr");
-//         row.innerHTML = `
-//             <td>${user.email}</td>
-//             <td>${user.password}</td>
-//             <td><button onclick="deleteUser(${index})">Delete</button></td>
-//         `;
-//         userTable.appendChild(row);
-//     });
+// function showError(element, message) {
+//   $(element).text(message).css("color", "red");
 // }
 
-
-// function addUser() {
-//     const newUser = {
-//         email: "newuser@example.com",
-//         password: "newpassword"
-//     };
-
-//     users.push(newUser);
-//     const newRow = document.createElement("tr");
-//     newRow.innerHTML = `
-//         <td>${newUser.email}</td>
-//         <td>${newUser.password}</td>
-//         <td><button onclick="deleteUser(${users.length - 1})">Delete</button></td>
-//     `;
-//     userTable.appendChild(newRow);
+// function clearErrorMessages() {
+//   $(".error-message").text("");
 // }
+// function validatePhoneNumber(phone) {
+//     return /^\d{10}$/.test(phone);
+//   }
 
+//   function validateName(str) {
+//     return /^[A-Za-z\s]+$/.test(str);
+//   }
 
-// function deleteUser(index) {
-//     users.splice(index, 1);
-//     userTable.innerHTML = ""; 
-//     createUserTable(); 
-// }
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     createEmployeeList();
-//     createUserTable();
-
-//     const addUserButton = document.getElementById("add-user-btn");
-//     addUserButton.addEventListener("click", addUser);
-
-// });
-
-
-
-   
-//   });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const userTable = document.getElementById("userTable");
-//     const addUserBtn = document.getElementById("addUserBtn");
-//     const newEmailInput = document.getElementById("newEmail");
-//     const newPasswordInput = document.getElementById("newPassword");
-  
-//     // Sample initial user data
-//     const initialUsers = [
-//       { email: "user1@example.com", password: "password123" },
-//       { email: "user2@example.com", password: "securepass" },
-//       // Add more initial users as needed
-//     ];
-  
-//     // Populate the table with initial user data
-//     initialUsers.forEach(user => {
-//       addUserToTable(user.email, user.password);
-//     });
-  
-//     // Function to add a user to the table
-//     function addUserToTable(email, password) {
-//       const newRow = userTable.insertRow();
-//       newRow.innerHTML = `
-//         <td>${email}</td>
-//         <td>${password}</td>
-//         <td><button class="deleteBtn">Delete</button></td>
-//       `;
-  
-//       const deleteBtn = newRow.querySelector(".deleteBtn");
-//       deleteBtn.addEventListener("click", function() {
-//         userTable.deleteRow(newRow.rowIndex);
-//       });
-//     }
-  
-//     // Add user button click event
-//     addUserBtn.addEventListener("click", function() {
-//       const newEmail = newEmailInput.value;
-//       const newPassword = newPasswordInput.value;
-//       if (newEmail && newPassword) {
-//         addUserToTable(newEmail, newPassword);
-//         newEmailInput.value = "";
-//         newPasswordInput.value = "";
-//       }
-//     });
-//   });
-
-  
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const SupplierTable = document.getElementById("supplierTable");
-//     const addSupplierBtn = document.getElementById("addSupplierBtn");
-//     const newEmailInput = document.getElementById("newEmail");
-//     const newPasswordInput = document.getElementById("newPassword");
-  
-//     // Sample initial user data
-//     const initialUsers = [
-//       { email: "supplier@example.com", password: "password123" },
-//       { email: "supplier2@example.com", password: "securepass" },
-//       // Add more initial users as needed
-//     ];
-  
-//     // Populate the table with initial user data
-//     initialUsers.forEach(user => {
-//         addSupplierToTable(user.email, user.password);
-//     });
-  
-//     // Function to add a user to the table
-//     function addSupplierToTable(email, password) {
-//       const newRow = SupplierTable.insertRow();
-//       newRow.innerHTML = `
-//         <td>${email}</td>
-//         <td>${password}</td>
-//         <td><button class="deleteBtn">Delete</button></td>
-//       `;
-  
-//       const deleteBtn = newRow.querySelector(".deleteBtn");
-//       deleteBtn.addEventListener("click", function() {
-//         SupplierTable.deleteRow(newRow.rowIndex);
-//       });
-//     }
-  
-//     // Add user button click event
-//     addSupplierBtn.addEventListener("click", function() {
-//       const newEmail = newEmailInput.value;
-//       const newPassword = newPasswordInput.value;
-//       if (newEmail && newPassword) {
-//         addSupplierToTable(newEmail, newPassword);
-//         newEmailInput.value = "";
-//         newPasswordInput.value = "";
-//       }
-//     });
-//   });
+//   function containsAtSymbol(inputString) {
+//     const pattern = /@/;
+//     return pattern.test(inputString);
+//   }
 
 
