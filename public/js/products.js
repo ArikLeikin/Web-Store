c131beba7b9abab0c79088bfd58930d85ea99389
Nofar Shapir <nofarshapir00@gmail.com>
2023-08-27 15:33:22 +0300

    fix products page

--- public/js/products.js
+++ public/js/products.js
@@ -1,100 +1,235 @@
+var max = 0;
+
 $(document).ready(function () {
-  // Function to handle sorting
-  function sortProducts() {
-    var $productGrid = $(".product-grid");
-    var selectedSortValue = $("#Sortby-filter").val();
+  const productGrid = $("#productGrid");
+  $("#category-title").text("All Games");
+  // Function to create the product HTML structure
+  function createProductHTML(product) {
+    const productItem = document.createElement("div");
+    productItem.className = "product-item";
+    productItem.setAttribute("data-age-range", product.ageRange);
+    productItem.setAttribute("data-game-type", product.gameType);
+    productItem.setAttribute("data-price", product.price);
+    productItem.setAttribute("filter-by", "");
 
-    var $productItems = $productGrid.find(".product-item");
+    // Creating the image container div
+    const imgContainer = document.createElement("div");
+    imgContainer.className = "img-container";
 
-    // Sort the product items based on the selected value
-    if (selectedSortValue === "best-selling") {
-      // Implement your best-selling sorting logic here
-    } else if (selectedSortValue === "price-high-to-low") {
-      $productItems.sort(function (a, b) {
-        var priceA = parseFloat($(a).data("price"));
-        var priceB = parseFloat($(b).data("price"));
-        return priceB - priceA;
-      });
-    } else if (selectedSortValue === "price-low-to-high") {
-      $productItems.sort(function (a, b) {
-        var priceA = parseFloat($(a).data("price"));
-        var priceB = parseFloat($(b).data("price"));
-        return priceA - priceB;
-      });
+    // Creating the product image link
+    const productLink = document.createElement("a");
+    productLink.className = "product-img";
+    productLink.href = "/product-details?id=" + product._id;
+
+    // Creating the product image element
+
+    if (product.image && product.image.length > 0) {
+      const imagePath = product.image[0].replace(/\\/g, "/");
+      const productImage = $("<img>")
+        .attr("src", imagePath)
+        .attr("alt", "item");
+      productLink.append(productImage);
+      imgContainer.append(productLink);
+      productItem.append(imgContainer);
     }
 
-    // Empty the grid and append sorted product items back to it
-    $productGrid.empty();
-    $productItems.appendTo($productGrid);
+    // Creating the product title section
+    const productTitleSection = document.createElement("section");
+    productTitleSection.className = "product-title";
+
+    // Creating the product title description
+    const productTitleDesc = document.createElement("span");
+    productTitleDesc.className = "product-title-desc";
+
+    // Creating the product title link
+    const productTitleLink = document.createElement("a");
+    productTitleLink.href = "#";
+    productTitleLink.textContent = product.title;
+
+    // Adding the title link to the title description
+    productTitleDesc.appendChild(productTitleLink);
+
+    // Adding the title description to the title section
+    productTitleSection.appendChild(productTitleDesc);
+
+    // Appending the title section to the product item
+    productItem.appendChild(productTitleSection);
+
+    // Creating the product price section
+    const productPriceSection = document.createElement("section");
+    productPriceSection.className = "product-price";
+
+    // Creating the product title price
+    const productTitlePrice = document.createElement("span");
+    productTitlePrice.className = "product-title-price center"; // Adding 'center' class
+    productTitlePrice.textContent = `$${product.price}`;
+
+    // Adding the price to the price section
+    productPriceSection.appendChild(productTitlePrice);
+
+    // Appending the price section to the product item
+    productItem.appendChild(productPriceSection);
+
+    if (max < product.price) {
+      max = product.price;
+      updatemaxprices();
+    }
+
+    return productItem;
   }
 
-  // Attach event handler to the dropdown change event
-  $("#Sortby-filter").on("change", function () {
-    sortProducts();
+  // Add an event listener to the min input field
+  $("#min-price-input").on("input", function () {
+    var minValue = parseFloat($(this).val()) || 0;
+    var currentMaxValue =
+      parseFloat($("#max-price-input").val()) || initialMaxValue;
+
+    // Update the max attribute of the max input field
+    $("#max-price-input").attr(
+      "min",
+      currentMaxValue < minValue ? currentMaxValue : minValue
+    );
   });
-});
 
-// get product api
-$(document).ready(function () {
-  $(document).ready(function () {
-    const productGrid = $("#productGrid");
+  // Add an event listener to the max input field
+  $("#max-price-input").on("input", function () {
+    var maxValue = parseFloat($(this).val()) || initialMaxValue;
+    var currentMinValue = parseFloat($("#min-price-input").val()) || 0;
 
-    function createProductHTML(product) {
-      const productItem = $("<div>").addClass("product-item");
-      productItem.attr({
-        "data-age-range": product.age_range,
-        "data-game-type": product.category,
-        "data-price": product.price.toFixed(2), // Format price to two decimal places
-        "filter-by": "",
-      });
+    // Update the max attribute of the min input field
+    $("#min-price-input").attr(
+      "max",
+      maxValue > currentMinValue ? maxValue : currentMinValue
+    );
+  });
 
-      const imgContainer = $("<div>").addClass("img-container");
-      const productLink = $("<a>").addClass("product-img").attr("href", "#");
+  function updatemaxprices() {
+    $("#max-price-input").attr("max", max + 1);
+    $("#max-price-input").val(parseInt(max + 1));
+    $("#min-price-input").attr(
+      "max",
+      parseInt($("#max-price-input").val()) - 1
+    );
+  }
 
-      if (product.image && product.image.length > 0) {
-        // Replace backslashes with forward slashes in image paths
-        const imagePath = product.image[0].replace(/\\/g, "/");
-        const productImage = $("<img>")
-          .attr("src", imagePath)
-          .attr("alt", "item");
-        productLink.append(productImage);
-        imgContainer.append(productLink);
-        productItem.append(imgContainer);
-      }
+  function applyFilters(products) {
+    const selectedGameType = $("#game-type-filter").val();
+    const selectedAge = $("#age-filter").val().toLowerCase(); // Convert to lowercase
+    const minPrice = parseFloat($("#min-price-input").val());
+    const maxPrice = parseFloat($("#max-price-input").val());
+    const selectedSorting = $("#Sortbyfilter").val();
 
-      const productTitleSection = $("<section>").addClass("product-title");
-      const productTitleDesc = $("<span>")
-        .addClass("product-title-desc")
-        .append($("<a>").attr("href", "#").text(product.title));
-      productTitleSection.append(productTitleDesc);
-      productItem.append(productTitleSection);
+    const filteredProducts = products.filter((product) => {
+      const filterCategory = selectedGameType;
+      const filterAge = product.age_range.toLowerCase(); // Convert to lowercase
+      const isGameTypeMatch =
+        !filterCategory ||
+        product.category.toLowerCase() === filterCategory.toLowerCase();
+      const isAgeRangeMatch = !selectedAge || filterAge === selectedAge;
+      const isPriceInRange =
+        (!minPrice || product.price >= minPrice) &&
+        (!maxPrice || product.price <= maxPrice);
+      return isGameTypeMatch && isAgeRangeMatch && isPriceInRange;
+    });
 
-      const productPriceSection = $("<section>").addClass("product-price");
-      const productTitlePrice = $("<span>")
-        .addClass("product-title-price center")
-        .text(`$${product.price.toFixed(2)}`); // Format price to two decimal places
-      productPriceSection.append(productTitlePrice);
-      productItem.append(productPriceSection);
+    sortProductsByPrice(filteredProducts, selectedSorting);
 
-      return productItem;
-    }
+    productGrid.empty();
+    filteredProducts.forEach((product) => {
+      const productHTML = createProductHTML(product);
+      productGrid.append(productHTML);
+    });
+  }
 
-    function displayProducts(products) {
-      productGrid.empty(); // Clear existing products
-      products.forEach((product) => {
-        const productHTML = createProductHTML(product);
-        productGrid.append(productHTML);
-      });
-    }
+  function sortProductsByPrice(products, sortOrder) {
+    products.sort((a, b) => {
+      const priceDifference = a.price - b.price;
+      return sortOrder === "highToLow" ? -priceDifference : priceDifference;
+    });
+  }
 
-    // Fetch products from the API
+  function fetchAndDisplayProducts() {
     const apiUrl = "http://127.0.0.1:8080/api/products";
-    $.get(apiUrl)
-      .done(function (data) {
-        displayProducts(data); // Display the fetched products
-      })
-      .fail(function () {
-        console.error("Error fetching products");
-      });
+
+    $.get(apiUrl, function (data, status) {
+      if (status === "success") {
+        applyFilters(data);
+      } else {
+        console.error("Error fetching products:", status);
+      }
+    }).fail(function () {
+      console.error("Network error occurred");
+    });
+  }
+
+  // Function to extract URL parameters
+  function getUrlParameter(name) {
+    const results = new RegExp("[?&]" + name + "=([^&#]*)").exec(
+      window.location.href
+    );
+    if (results == null) {
+      return null;
+    } else {
+      return decodeURI(results[1]) || 0;
+    }
+  }
+
+  // Extract 'category' parameter from the URL
+  const urlCategory = getUrlParameter("category");
+  if (urlCategory) {
+    const optionValues = [
+      { value: "", label: "All" },
+      { value: "boardgames", label: "Board Games" },
+      { value: "ridingtoys", label: "Riding Toys" },
+      { value: "books", label: "Books" },
+      { value: "science", label: "Sience" },
+      { value: "puzzles", label: "Puzzle Games" },
+      { value: "lego", label: "Lego" },
+      { value: "games", label: "Games" },
+      { value: "musickaroke", label: "Music & Karaoke" },
+      { value: "dolls", label: "Dolls" },
+      { value: "outdoor", label: "Outdoor" },
+      { value: "yad2", label: "Yad 2" },
+    ];
+    const selectedOption = optionValues.find(
+      (option) => option.value.toLowerCase() === urlCategory.toLowerCase()
+    );
+
+    $("#game-type-filter").val(selectedOption.value);
+    // $("#game-type-filter").prop("disabled", true);
+    $("#category-title").text(selectedOption.label);
+
+    fetchAndDisplayProducts();
+  }
+
+  // Extract 'category' parameter from the URL
+  const urlAge = getUrlParameter("age");
+  if (urlAge) {
+    const optionValues = [
+      { value: "", label: "All" },
+      { value: "0-12", label: "0-12 months" },
+      { value: "12-24", label: "12-24 months" },
+      { value: "2-4", label: "2-4 Years" },
+      { value: "5-7", label: "5-7 Years" },
+      { value: "8-99", label: "8-99 Years" },
+    ];
+    const selectedOption = optionValues.find(
+      (option) => option.value.toLowerCase() === urlAge.toLowerCase()
+    );
+
+    $("#age-filter").val(selectedOption.value);
+    // $("#age-filter").prop("disabled", true);
+
+    fetchAndDisplayProducts();
+  }
+
+  $("#filter-button").click(function () {
+    fetchAndDisplayProducts();
   });
+
+  $("#Sortbyfilter").change(function () {
+    fetchAndDisplayProducts();
+  });
+
+  fetchAndDisplayProducts(); // Initial fetch and display
 });
