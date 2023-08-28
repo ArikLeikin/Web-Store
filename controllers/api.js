aeb02713ed8e7fe375e10cd9218a0c90f152dfe2
ArikLeikin <arik.leikin1998@gmail.com>
2023-08-28 22:30:26 +0300

    Added api fix

--- controllers/api.js
+++ controllers/api.js
@@ -25,7 +25,9 @@ module.exports = {
   },
   getCurrentUser: async (req, res) => {
     try {
-      const user = await User.findById(req.session.user._id);
+      const user = await User.findById(req.session.user._id).populate(
+        "usedProducts"
+      );
       res.status(200).json(user);
     } catch (err) {
       res.status(500).json({ message: "Error retrieving user", error: err });
