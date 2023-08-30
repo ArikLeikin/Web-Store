f56e1f44b353014016d367016316184ce0a2e1d8
arik-leikin-hs <arik.l@hiredscore.com>
2023-08-30 19:31:07 +0300

    Fixed charts coloring

--- public/js/d3.js
+++ public/js/d3.js
@@ -1,12 +1,26 @@
+
 // const serverEndpoint = 'http://127.0.0.1:8080/api/products';
 
 // // Fetch product data from the server
 // fetch(serverEndpoint)
 //   .then(response => response.json())
 //   .then(products => {
-//     console.log(products);
-//     var xValues = products.map(product => product.category);
-//     var yValues = products.map(product => product.quantity);
+//     // Create a map to aggregate quantities for each category
+//     const categoryQuantities = new Map();
+
+//     // Iterate through product data to aggregate quantities
+//     products.forEach(product => {
+//       const { category, quantity } = product;
+//       if (categoryQuantities.has(category)) {
+//         categoryQuantities.set(category, categoryQuantities.get(category) + quantity);
+//       } else {
+//         categoryQuantities.set(category, quantity);
+//       }
+//     });
+
+//     // Extract aggregated category names and quantities
+//     var xValues = Array.from(categoryQuantities.keys());
+//     var yValues = Array.from(categoryQuantities.values());
 //     var barColors = ["red", "green", "blue", "orange", "brown"];
 
 //     new Chart("myChart", {
@@ -31,6 +45,7 @@
 //     console.error("Error fetching product data:", error);
 //   });
 
+
 const serverEndpoint = 'http://127.0.0.1:8080/api/products';
 
 // Fetch product data from the server
@@ -53,7 +68,12 @@ fetch(serverEndpoint)
     // Extract aggregated category names and quantities
     var xValues = Array.from(categoryQuantities.keys());
     var yValues = Array.from(categoryQuantities.values());
-    var barColors = ["red", "green", "blue", "orange", "brown"];
+    var barColors = [];
+
+    // Alternate between two colors for each column
+    for (let i = 0; i < xValues.length; i++) {
+      barColors.push(i % 2 === 0 ? "blue" : "orange");
+    }
 
     new Chart("myChart", {
       type: "bar",
--- public/js/d3_2.js
+++ public/js/d3_2.js
@@ -1,3 +1,55 @@
+// const serverEndpoint = 'http://127.0.0.1:8080/api/users';
+
+// // Fetch user data from the server
+// fetch(serverEndpoint)
+//   .then(response => response.json())
+//   .then(users => {
+//     // Create an object to count users for each category
+//     const usersPerCategory = {
+//       "0 orders": 0,
+//       "1-5 orders": 0,
+//       "more than 5 orders": 0
+//     };
+
+//     // Iterate through user data to count users per category
+//     users.forEach(user => {
+//         console.log(user.email + " : " + user.orderHistory.length);
+//       const orderCount = user.orderHistory.length;
+//       if (orderCount === 0) {
+//         usersPerCategory["0 orders"]++;
+//       } else if (orderCount <= 5) {
+//         usersPerCategory["1-5 orders"]++;
+//       } else {
+//         usersPerCategory["more than 5 orders"]++;
+//       }
+//     });
+
+//     // Extract categories and user counts
+//     const categories = Object.keys(usersPerCategory);
+//     const userCounts = Object.values(usersPerCategory);
+//     const barColors = ["blue", "orange", "blue"];
+
+//     new Chart("myChart", {
+//       type: "bar",
+//       data: {
+//         labels: categories,
+//         datasets: [{
+//           backgroundColor: barColors,
+//           data: userCounts
+//         }]
+//       },
+//       options: {
+//         legend: { display: false },
+//         title: {
+//           display: true,
+//           text: "User Order Distribution: Number of Users per Order Category"
+//         }
+//       }
+//     });
+//   })
+//   .catch(error => {
+//     console.error("Error fetching user data:", error);
+//   });
 const serverEndpoint = 'http://127.0.0.1:8080/api/users';
 
 // Fetch user data from the server
@@ -13,7 +65,7 @@ fetch(serverEndpoint)
 
     // Iterate through user data to count users per category
     users.forEach(user => {
-        console.log(user.email + " : " + user.orderHistory.length);
+        console.log(user.email + " : " + user.orderHistory.lengthc);
       const orderCount = user.orderHistory.length;
       if (orderCount === 0) {
         usersPerCategory["0 orders"]++;
@@ -27,7 +79,9 @@ fetch(serverEndpoint)
     // Extract categories and user counts
     const categories = Object.keys(usersPerCategory);
     const userCounts = Object.values(usersPerCategory);
-    const barColors = ["red", "green", "blue"];
+    
+    // Alternating bar colors
+    const barColors = userCounts.map((count, index) => index % 2 === 0 ? "blue" : "orange");
 
     new Chart("myChart", {
       type: "bar",
