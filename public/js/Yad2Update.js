d29f929615572fa0f75adc966f130a0bc8230381
Nofar Shapir <nofarshapir00@gmail.com>
2023-08-30 16:16:16 +0300

    change validation

--- public/js/Yad2Update.js
+++ public/js/Yad2Update.js
@@ -1,6 +1,5 @@
 function validateLettersDigitsAndSpaces(str) {
-  //return /^[a-zA-Z0-9 ]{4,}$/.test(str);
-  return /^[a-zA-Z0-9\s\S]{5,}$/.test(str);
+  return /^[a-zA-Z0-9 ]{4,}$/.test(str);
 }
 function containsOnlyNumbers(str) {
   return /^\d+$/.test(str);
@@ -17,16 +16,63 @@ function clearErrorMessages() {
   $(".error-message").text("");
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+  const deleteButton = document.querySelector(".delete-btn");
+  const productId = window.location.search.split("=")[1];
+
+  deleteButton.addEventListener("click", function () {
+    deleteProduct(productId);
+  });
+
+  function deleteProduct(id) {
+    fetch(`http://127.0.0.1:8080/delete/product/${id}`, {
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("success");
+        window.location.href = "http://127.0.0.1:8080/products?category=yad2";
+      })
+      .catch((error) => {
+        console.error("Error deleting product:", error);
+      });
+  }
+});
+
+/* ~~~~~~~~~~~~~~~~get api ~~~~~~~~~~~~~~~~~~~*/
+
 $(document).ready(function () {
   const productId = window.location.search.split("=")[1];
   console.log(productId);
+
+  $.ajax({
+    url: `http://127.0.0.1:8080/api/product/${productId}`,
+    method: "GET",
+    dataType: "json",
+    success: function (data) {
+      console.log(data.data);
+
+      $("#productName").val(data.data.title);
+      $("#condition").val(data.data.condition);
+      $("#price").val(data.data.price);
+      $("#age").val(data.data.age_range);
+      $("#category").val(data.data.category);
+      $("#description").val(data.data.description);
+    },
+    error: function (error) {
+      console.error("Error fetching product data:", error);
+    },
+  });
+});
+
+/*  ~~~~~~~~~~~~~~~~~ post api ~~~~~~~~~~~~~~~~~~~ */
+$(document).ready(function () {
   $("#UpdateYad2-form").submit(function (event) {
     event.preventDefault();
     clearErrorMessages();
 
     let isValid = true;
     var price = $("#price").val();
-    //var bankAccount = $("#bankAccount").val();
     var productName = $("#productName").val();
 
     if (!validateLettersDigitsAndSpaces(productName)) {
@@ -45,9 +91,30 @@ $(document).ready(function () {
         title: "Validation Error",
         text: "Please enter a valid price (numbers only and greater than zero).",
       });
-
+      // showError(
+      //   "#price-error",
+      //   "Please enter a valid price (numbers only and greater than zero)."
+      // );
       isValid = false;
     }
+
+    var productPhotos = $("#productPhotos")[0].files;
+    // if (productPhotos.length === 0) {
+    //   Swal.fire({
+    //     icon: 'warning',
+    //     title: 'Validation Error',
+    //     text: 'Please upload at least 1 photo.',
+    //   });
+    //   isValid = false;
+    // } else if (productPhotos.length > 4) {
+    //   Swal.fire({
+    //     icon: 'warning',
+    //     title: 'Validation Error',
+    //     text: 'You can upload a maximum of 4 photos.',
+    //   });
+    //   isValid = false;
+
+    // }
 
     if (isValid) {
       event.preventDefault();
@@ -87,49 +154,6 @@ $(document).ready(function () {
           console.error("Error updating product:", error);
         },
       });
-
-      $.ajax({
-        url: `http://127.0.0.1:8080/api/product/${productId}`,
-        method: "GET",
-        dataType: "json",
-        success: function (data) {
-          console.log(data.data);
-
-          $("#productName").val(data.data.title);
-          $("#condition").val(data.data.condition);
-          $("#price").val(data.data.price);
-          $("#age").val(data.data.age_range);
-          //$('#bankAccount').val(data.data.bankAccount);
-          $("#category").val(data.data.category);
-          $("#description").val(data.data.description);
-        },
-        error: function (error) {
-          console.error("Error fetching product data:", error);
-        },
-      });
     }
   });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const deleteButton = document.querySelector(".delete-btn");
-  const productId = window.location.search.split("=")[1];
-
-  deleteButton.addEventListener("click", function () {
-    deleteProduct(productId);
-  });
-
-  function deleteProduct(id) {
-    fetch(`http://127.0.0.1:8080/delete/product/${id}`, {
-      method: "POST",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("success");
-        window.location.href = "http://127.0.0.1:8080/products?category=yad2";
-      })
-      .catch((error) => {
-        console.error("Error deleting product:", error);
-      });
-  }
 });
